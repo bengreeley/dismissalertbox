efffb41745ec830d93309221095cba0f3455e71c
Ben Greeley <ben.greeley@gmail.com>
2015-02-26 10:32:16 -0500

    Deactivation code added

--- inc/class-dismissAlertBox-deactivator.php
+++ inc/class-dismissAlertBox-deactivator.php
@@ -4,6 +4,7 @@
 class dismissAlertBox_Deactivator {
 
 	public static function deactivate() {
-
+		unregister_setting( 'dismissalertbox-group', 'dismissalertbox_hideonclose' );
+		unregister_setting( 'dismissalertbox-group', 'dismissalertbox_preventanimation' );
 	}
 }
